b25bb44ea04f9bc2921eded88b8ff7ac2879fc45
abcdlsj <lisongjianshuai@gmail.com>
2021-11-17 19:58:25 +0800

    update graphql node

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,44 @@
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
+
+let links = [
+	{
+		id: 'link-0',
+		url: 'www.howtographql.com',
+		description: 'Fullstack tutorial for GraphQL'
+	}
+];
+
+// 2
+const resolvers = {
+	Query: {
+		info: () => `This is the API of a Hackernews Clone`,
+		feed: () => links
+	},
+    Mutation: {
+        post: (parent, args) => {
+            let idCount = links.length
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link);
+            return link
+        }
+    },
+	Link: {
+		id: (parent) => parent.id,
+		description: (parent) => parent.description,
+		url: (parent) => parent.url
+	}
+};
+
+// 3
+const server = new ApolloServer({
+	typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
+	resolvers
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
