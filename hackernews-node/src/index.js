d12ab674aab771c41aa218f87f24b1cd22a71ec4
abcdlsj <lisongjianshuai@gmail.com>
2021-12-05 22:13:17 +0800

    update

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,44 +1,62 @@
-const { ApolloServer } = require('apollo-server');
-const fs = require('fs');
-const path = require('path');
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const { link } = require("fs/promises");
+const path = require("path");
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 let links = [
-	{
-		id: 'link-0',
-		url: 'www.howtographql.com',
-		description: 'Fullstack tutorial for GraphQL'
-	}
+    {
+        id: "link-0",
+        url: "www.howtographql.com",
+        description: "Fullstack tutorial for GraphQL",
+    },
 ];
 
 // 2
 const resolvers = {
-	Query: {
-		info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links
-	},
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
     Mutation: {
         post: (parent, args) => {
-            let idCount = links.length
+            let idCount = links.length;
             const link = {
                 id: `link-${idCount++}`,
                 description: args.description,
                 url: args.url,
-            }
+            };
             links.push(link);
-            return link
-        }
+            return link;
+        },
+        update: (parent, args) => {
+            links.forEach(function (item, index, array) {
+                if (item.id == args.id) {
+                    array[index].url = args.url;
+                    array[index].description = args.description;
+                }
+            });
+        },
+        delete: (parent, args) => {
+            links.forEach(function (item, index, array) {
+                if (item.id == args.id) {
+                    array.splice(index, 1)
+                }
+            });
+        },
     },
-	Link: {
-		id: (parent) => parent.id,
-		description: (parent) => parent.description,
-		url: (parent) => parent.url
-	}
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+    },
 };
 
 // 3
 const server = new ApolloServer({
-	typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
-	resolvers
+    typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+    resolvers,
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
